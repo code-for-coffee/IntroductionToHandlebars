7c8f0f8b2e8837013872b1a8ec9fff7cd262f38c
James Traver <jtraverjr@gmail.com>
2015-05-19 15:47:12 -0500

    2.7

--- 2-Building_a_template/example/Resources/Scripts/app.js
+++ 2-Building_a_template/example/Resources/Scripts/app.js
@@ -1,6 +1,14 @@
-//app.js
-var app = app || {};
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['aboutMe.hbs'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-app.renderTemplate = function(template, data) {
-  
-};
+  return "<article>\nHi, my name is "
+    + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
+    + "! My favourite colour is "
+    + alias3(((helper = (helper = helpers.colour || (depth0 != null ? depth0.colour : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"colour","hash":{},"data":data}) : helper)))
+    + " and I enjoy eating at "
+    + alias3(((helper = (helper = helpers.restaurant || (depth0 != null ? depth0.restaurant : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"restaurant","hash":{},"data":data}) : helper)))
+    + "!\n</article>\n";
+},"useData":true});
+})();
